d1512c7f69111223bca92aff8293a3c8f5835f3a
James <jbarks789@yahoo.com>
2022-10-22 15:50:55 -0500

    commit feature 3 changes

--- map-mania-lite.js
+++ map-mania-lite.js
@@ -1,3 +1,12 @@
+
+var gMap;
+
+function initMap() {
+    // create a new map and assign it to gMap
+    gMap = new google.maps.Map(document.getElementById('myMapID'), {
+        center: {lat: 41.878, lng: 10}, zoom: 3});
+}
+
 function initApplication() {
     console.log("Map Mania Lite - Starting");
 }
